078c083239038c626e990d1f9b6d87cc53949209
Max Flintoff <max.flintoff@ibm.com>
2020-03-16 18:00:29 +0000

    added portworx ports to sg

--- modules/worker_node/worker_sg.tf
+++ modules/worker_node/worker_sg.tf
@@ -35,11 +35,34 @@ resource "ibm_security_group_rule" "worker_ssh" {
     security_group_id = ibm_security_group.worker_private_sg.id
 }
 
+resource "ibm_security_group_rule" "worker_portworx" {
+    direction = "ingress"
+    port_range_min = 9001
+    port_range_max = 9022
+    protocol = "tcp"
+    security_group_id = ibm_security_group.worker_private_sg.id
+}
+
+resource "ibm_security_group_rule" "worker_portworx_udp" {
+    direction = "ingress"
+    port_range_min = 9002
+    port_range_max = 9002
+    protocol = "udp"
+    security_group_id = ibm_security_group.worker_private_sg.id
+}
+
+resource "ibm_security_group_rule" "worker_portworx_lighthouse" {
+    direction = "ingress"
+    port_range_min = 32678
+    port_range_max = 32679
+    protocol = "tcp"
+    security_group_id = ibm_security_group.worker_private_sg.id
+}
+
 resource "ibm_security_group_rule" "egress" {
     direction = "egress"
     security_group_id = ibm_security_group.worker_private_sg.id
 }
-
 
 
 resource "ibm_security_group" "worker_public_sg" {
@@ -59,6 +82,30 @@ resource "ibm_security_group_rule" "worker_ssh_public" {
     direction = "ingress"
     port_range_min = 22
     port_range_max = 22
+    protocol = "tcp"
+    security_group_id = ibm_security_group.worker_public_sg.id
+}
+
+resource "ibm_security_group_rule" "worker_portworx_public" {
+    direction = "ingress"
+    port_range_min = 9001
+    port_range_max = 9022
+    protocol = "tcp"
+    security_group_id = ibm_security_group.worker_public_sg.id
+}
+
+resource "ibm_security_group_rule" "worker_portworx_udp_public" {
+    direction = "ingress"
+    port_range_min = 9002
+    port_range_max = 9002
+    protocol = "udp"
+    security_group_id = ibm_security_group.worker_public_sg.id
+}
+
+resource "ibm_security_group_rule" "worker_portworx_lighthouse_public" {
+    direction = "ingress"
+    port_range_min = 32678
+    port_range_max = 32679
     protocol = "tcp"
     security_group_id = ibm_security_group.worker_public_sg.id
 }
